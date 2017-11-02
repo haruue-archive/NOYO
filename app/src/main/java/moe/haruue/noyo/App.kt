1c03928b986e6b6e7e28593c02b3169859a309a8
Haruue Icymoon <haruue@caoyue.com.cn>
2017-11-02 23:28:24 +0800

    initialize: project-noyo
      This is a app for Wu Yutong's science research task of CQUPT
      affiliated by Xie Shaoci
    
      This commit includes extra things:
    
        - app_name=hoyo
        - ic_launcher
        - SplashActivity

---
+++ app/src/main/java/moe/haruue/noyo/App.kt
@@ -0,0 +1,10 @@
+package moe.haruue.noyo
+
+import android.app.Application
+
+/**
+ *
+ * @author Haruue Icymoon haruue@caoyue.com.cn
+ */
+class App : Application() {
+}
---
+++ app/src/main/java/moe/haruue/noyo/SplashActivity.kt
@@ -0,0 +1,17 @@
+package moe.haruue.noyo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ *
+ * @author Haruue Icymoon haruue@caoyue.com.cn
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
